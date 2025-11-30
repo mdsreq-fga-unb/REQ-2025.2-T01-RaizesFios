e0876d56dbe476886f3394f814e695506e7a6867
Giovanni Ferreira <giovannidornelasf@gmail.com>
2025-11-30 14:30:27 -0300

    Pequena alteração nas routes

--- backend/src/api/routes/index.ts
+++ backend/src/api/routes/index.ts
@@ -3,6 +3,8 @@ import healthController from "../controllers/health.controller";
 import userRouter from "./user.routes";
 import authRouter from "./auth.routes";
 import addressRouter from "./address.routes";
+import productRouter from "./product.routes"; 
+import categoryRoutes from "./category.routes";
 
 const router = Router();
 
@@ -10,5 +12,7 @@ router.get("/health", healthController.check);
 router.use("/users", userRouter);
 router.use("/auth", authRouter);
 router.use("/addresses", addressRouter);
+router.use("/products", productRouter); 
+router.use("/categories", categoryRoutes);
 
 export default router;
